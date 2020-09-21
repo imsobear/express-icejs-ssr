c83d6cd95ab0b5667b2559d81deee1ff77699a28
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-09-21 13:52:59 +0800

    chore: 升级正式版本

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,15 @@
-var express = require('express');
 const fs = require('fs');
 const path = require('path');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  // 后续会去除对 htmlTemplate 的依赖
-  const htmlTemplate = fs.readFileSync(path.join(__dirname, '../../build/index.html'), 'utf8');
-
   const render = require('../../build/server/index').default;
   const { html, error } = await render({
-    // 当 前请求的路径（必选参数）
+    // 当前请求的路径（必选参数）
     pathname: req.path,
     initialData: {},
-    htmlTemplate,
   });
 
   if (error) {
